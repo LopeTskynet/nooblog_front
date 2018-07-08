619d9a0bba6621979be77f6a71c522532b1808ac
tskynet <stephdumonceau@gmail.com>
2018-07-08 12:02:17 +0200

    insert the request for create an article, and title and tag param for article

--- src/components/Article/ArticleCreation.vue
+++ src/components/Article/ArticleCreation.vue
@@ -1,14 +1,27 @@
 <template>
   <!-- eslint-disable -->
   <div id="articleCreation">
-    test
+    <div v-if="!this.preview">
+      <v-text-field
+        id="title"
+        v-model="title"
+        label="Title of this article"
+        required>
+      </v-text-field>
+      <v-text-field
+        id="tag"
+        v-model="tag"
+        label="Enter tags here, you must separate tags by a space like this : ' ' "
+        required>
+      </v-text-field>
+    </div>
     <div v-if="!this.preview">
       <v-form ref="form" lazy-validation>
         <wysiwyg id="myEditor" v-model="myHTML" />
       </v-form>
     </div>
     <div v-else>
-      <ArticlePreview v-bind:article="this.test"></ArticlePreview>
+      <ArticlePreview v-bind:article="this.article" v-bind:title="this.title" v-bind:tag="this.tag"></ArticlePreview>
     </div>
     <v-btn @click="saveArticle">
       <span v-if="!this.preview">Show the preview</span>
@@ -28,13 +41,20 @@ export default {
   data: () => ({
     myHTML: '',
     preview: false,
-    test: 'tes2t'
+    title: 'te',
+    tag: 'te',
+    article: ''
   }),
   methods: {
     saveArticle () {
       // console.log(document.getElementById('myEditor').childNodes[1].innerHTML)
       if (this.preview === false) {
-        this.test = document.getElementById('myEditor').childNodes[1].innerHTML
+        this.article = document.getElementById('myEditor').childNodes[1].innerHTML
+        this.title = document.getElementById('title').value
+        this.tag = document.getElementById('tag').value
+        console.log(document.getElementById('title').innerHTML)
+        this.tag = this.tag.split(' ')
+        console.log(this.tag)
         this.preview = true
       } else {
         this.preview = false
--- src/components/Article/ArticlePreview.vue
+++ src/components/Article/ArticlePreview.vue
@@ -1,24 +1,62 @@
 <template>
   <!-- eslint-disable -->
   <div id="articlePreview">
-  <pre v-html="article">
-    {{ article }}
-  </pre>
+    <div>
+      <h3>Title :</h3>{{ title }}
+    </div>
+    <div>
+      <h3>Tags :</h3>
+      <span v-for="value in tag"> {{ value }} </span>
+    </div>
+    <h3>Article :</h3>
+    <div v-html="article">
+      {{ article }}
+    </div>
+    <v-btn @click="submit">test</v-btn>
   </div>
   <!-- eslint-enable -->
 </template>
 
 <script>
+import axios from 'Axios'
 export default {
   name: 'ArticlePreview',
   props: {
     'article': {
       type: String,
       required: true
+    },
+    'title': {
+      type: String,
+      required: true
+    },
+    'tag': {
+      type: Array,
+      required: true
     }
   },
   data: () => ({
-  })
+  }),
+  methods: {
+    submit () {
+      if (this.article) {
+        console.log(this.article)
+        axios.post('http://localhost:3000/api/v1/article', {
+          article: this.article,
+          title: this.title,
+          tag: this.tag
+        }).then(response => {
+          if (response.data) {
+            console.log(response.data)
+          } else {
+            console.log('error')
+          }
+        }).catch(err => {
+          console.error(err)
+        })
+      }
+    }
+  }
 }
 </script>
 
