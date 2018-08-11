b5b6297f8ac41dab2263c935b7ed8b092b2e2bdc
tskynet <stephdumonceau@gmail.com>
2018-08-12 00:47:20 +0200

    bind the form with v-model, add btn delete, add methods for add and delete item

--- src/components/TechnicalSheet/TechnicalSheetCreation.vue
+++ src/components/TechnicalSheet/TechnicalSheetCreation.vue
@@ -3,13 +3,14 @@
     <div class="container">
       <h2>Création de fiche technique</h2>
       <v-form ref="form">
-        <v-text-field label="Nom de la fiche technique" required />
+        <v-text-field label="Nom de la fiche technique" required v-model="technicalSheet.name" />
         <v-textarea
           name="pharma"
           box
           label="Pharmacologie"
           auto-grow
           placeholder="Remplir les informations sur la pharmacologie ici."
+          v-model="technicalSheet.pharmacologie"
         />
         <v-textarea
           name="chimie"
@@ -17,6 +18,7 @@
           label="Chimie"
           auto-grow
           placeholder="Remplir les informations sur la chimie ici."
+          v-model="technicalSheet.chimie"
         />
 
         <v-expansion-panel>
@@ -27,16 +29,17 @@
             <v-card>
               <v-card-text>
                 <ul class="listNameEffect">
-                  <li v-for="item in inputPhysicEffect" :key="item">
+                  <li v-for="item in technicalSheet.effects.physic.counter" :key="item">
                     <h4>Effet n°{{ item }}</h4>
-                    <v-text-field placeholder="Nom de l'effet" :name="setName(effects[0], item)" v-model="nameEffectPhysic[item]" />
+                    <v-text-field placeholder="Nom de l'effet" :name="setName(effects[0], item)" v-model="technicalSheet.effects.physic.name[item]" />
                     <v-textarea
                       box
                       :name="setDescribeName(effects[0], item)"
                       auto-grow
-                      v-model="describeEffectPhysic[item]"
+                      v-model="technicalSheet.effects.physic.describe[item]"
                       placeholder="Remplir la description ici."
                     />
+                    <v-btn class="btnDelete" @click="deletePhysicEffect(item)">X</v-btn>
                   </li>
                   <v-btn @click="addPhysicEffect">Ajout d'un effet physique</v-btn>
                 </ul>
@@ -51,16 +54,17 @@
             <v-card>
               <v-card-text>
                 <ul class="listNameEffect">
-                  <li v-for="item in inputCognitifEffect" :key="item">
+                  <li v-for="item in technicalSheet.effects.cognitif.counter" :key="item">
                     <h4>Effet n°{{ item }}</h4>
-                    <v-text-field placeholder="Nom de l'effet" :name="setName(effects[1], item)" v-model="nameEffectCognitif[item]" />
+                    <v-text-field placeholder="Nom de l'effet" :name="setName(effects[1], item)" v-model="technicalSheet.effects.cognitif.name[item]" />
                     <v-textarea
                       box
                       :name="setDescribeName(effects[1], item)"
                       auto-grow
-                      v-model="describeEffectCognitif[item]"
+                      v-model="technicalSheet.effects.cognitif.describe[item]"
                       placeholder="Remplir la description ici."
                     />
+                    <v-btn class="btnDelete" @click="deleteCognitifEffect(item)">X</v-btn>
                   </li>
                   <v-btn @click="addCognitifEffect">Ajout d'un effet cognitif</v-btn>
                 </ul>
@@ -75,16 +79,17 @@
             <v-card>
               <v-card-text>
                 <ul class="listNameEffect">
-                  <li v-for="item in inputIndesirableEffect" :key="item">
+                  <li v-for="item in technicalSheet.effects.indesirable.counter" :key="item">
                     <h4>Effet n°{{ item }}</h4>
-                    <v-text-field placeholder="Nom de l'effet" :name="setName(effects[2], item)" v-model="nameEffectIndesirable[item]" />
+                    <v-text-field placeholder="Nom de l'effet" :name="setName(effects[2], item)" v-model="technicalSheet.effects.indesirable.name[item]" />
                     <v-textarea
                       box
                       :name="setDescribeName(effects[2], item)"
                       auto-grow
-                      v-model="describeEffectIndesirable[item]"
+                      v-model="technicalSheet.effects.indesirable.describe[item]"
                       placeholder="Remplir la description ici."
                     />
+                    <v-btn class="btnDelete" @click="deleteIndesirableEffect(item)">X</v-btn>
                   </li>
                   <v-btn @click="addIndesirableEffect">Ajout d'un effet indésirable</v-btn>
                 </ul>
@@ -99,11 +104,13 @@
           label="Toxicité et Interaction potentielle"
           auto-grow
           placeholder="Remplir les informations sur la toxicité et les interactions ici."
+          v-model="technicalSheet.toxicity"
         />
 
-        <ul class="listNameEffect">
-          <li v-for="item in inputUrlReference" :key="item">
-            <v-text-field placeholder="Entrez l'url ici" :name="setUrlName(item)" />
+        <ul class="listUrlReference">
+          <li v-for="item in technicalSheet.references.counter" :key="item">
+            <v-text-field placeholder="Entrez l'url ici" :name="setUrlName(item)" v-model="technicalSheet.references.urlTab[item]" />
+            <v-btn class="btnDelete" @click="deleteUrlReference(item)">X</v-btn>
           </li>
           <v-btn @click="addUrlReference">Ajout d'une référence</v-btn>
         </ul>
@@ -121,25 +128,65 @@ export default {
     inputIndesirableEffect: 1,
     inputUrlReference: 1,
     effects: ['physicEffect', 'cognitifEffect', 'indesirableEffect'],
-    nameEffectPhysic: [],
-    describeEffectPhysic: [],
-    nameEffectCognitif: [],
-    describeEffectCognitif: [],
-    nameEffectIndesirable: [],
-    describeEffectIndesirable: []
+    technicalSheet: {
+      name: '',
+      pharmacologie: '',
+      chimie: '',
+      effects: {
+        physic: {
+          name: [],
+          describe: [],
+          counter: 1
+        },
+        cognitif: {
+          name: [],
+          describe: [],
+          counter: 1
+        },
+        indesirable: {
+          name: [],
+          describe: [],
+          counter: 1
+        }
+      },
+      toxicity: '',
+      references: {
+        urlTab: [],
+        counter: 1
+      }
+    }
   }),
   methods: {
     addPhysicEffect () {
-      this.inputPhysicEffect = this.inputPhysicEffect + 1
+      this.technicalSheet.effects.physic.counter = this.technicalSheet.effects.physic.counter + 1
     },
     addCognitifEffect () {
-      this.inputCognitifEffect = this.inputCognitifEffect + 1
+      this.technicalSheet.effects.cognitif.counter = this.technicalSheet.effects.cognitif.counter + 1
     },
     addIndesirableEffect () {
-      this.inputIndesirableEffect = this.inputIndesirableEffect + 1
+      this.technicalSheet.effects.indesirable.counter = this.technicalSheet.effects.indesirable.counter + 1
     },
     addUrlReference () {
-      this.inputUrlReference = this.inputUrlReference + 1
+      this.technicalSheet.references.counter = this.technicalSheet.references.counter + 1
+    },
+    deletePhysicEffect (index) {
+      this.technicalSheet.effects.physic.counter = this.technicalSheet.effects.physic.counter - 1
+      this.technicalSheet.effects.physic.name.splice([index], 1)
+      this.technicalSheet.effects.physic.describe.splice([index], 1)
+    },
+    deleteCognitifEffect (index) {
+      this.technicalSheet.effects.cognitif.counter = this.technicalSheet.effects.cognitif.counter - 1
+      this.technicalSheet.effects.cognitif.name.splice([index], 1)
+      this.technicalSheet.effects.cognitif.describe.splice([index], 1)
+    },
+    deleteIndesirableEffect (index) {
+      this.technicalSheet.effects.indesirable.counter = this.technicalSheet.effects.indesirable.counter - 1
+      this.technicalSheet.effects.indesirable.name.splice([index], 1)
+      this.technicalSheet.effects.indesirable.describe.splice([index], 1)
+    },
+    deleteUrlReference (index) {
+      this.technicalSheet.references.counter = this.technicalSheet.references.counter - 1
+      this.technicalSheet.references.urlTab.splice([index], 1)
     },
     setName (type, str) {
       return type + str
@@ -147,34 +194,53 @@ export default {
     setDescribeName (type, str) {
       return type + 'Describe' + str
     },
-    testing (str) {
-      return 'effect' + str
-    },
     setUrlName (str) {
       return 'urlName' + str
     },
     testForm () {
-      console.log('PHYSIC EFFECT')
-      console.log(this.nameEffectPhysic)
-      console.log(this.describeEffectPhysic)
-      console.log('\n \n COGNITIF EFFECT')
-      console.log(this.nameEffectCognitif)
-      console.log(this.describeEffectCognitif)
-      console.log('\n \n INDESIRABLE')
-      console.log(this.nameEffectIndesirable)
-      console.log(this.describeEffectIndesirable)
+      console.log(this.technicalSheet)
     }
   }
 }
 </script>
 <style scoped>
-#technicalSheetCreation .listNameEffect{
-  list-style:none;
-}
 #technicalSheetCreation .listNameEffect li {
   margin:10px 0 10px 0;
   padding : 5px 10px 5px 10px;
   border:2px solid black;
   border-radius: 8px;
+  position: relative;
+  list-style:none;
+}
+#technicalSheetCreation .listNameEffect li .btnDelete {
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  margin:0;
+  padding:0;
+  width:20px;
+  height:20px;
+  min-width:10px;
+  background:black;
+  color:white;
+  border-radius:50%;
+}
+#technicalSheetCreation .listUrlReference li {
+  list-style:none;
+  padding-left:30px;
+  position:relative;
+}
+#technicalSheetCreation .listUrlReference li .btnDelete {
+  position: absolute;
+  left: 5px;
+  top:5px;
+  margin:0;
+  padding:0;
+  width:20px;
+  height:20px;
+  min-width:10px;
+  background:black;
+  color:white;
+  border-radius:50%;
 }
 </style>
