59d4a83a9fee4b44ee8316d29e71c4e6070e9e3a
DylanBourdere <loperanes@gmail.com>
2018-08-23 22:32:12 +0200

    Add all technicalsheet in vue still need to fix some bug

--- src/components/TechnicalSheet/TechnicalSheetAll.vue
+++ src/components/TechnicalSheet/TechnicalSheetAll.vue
@@ -1,19 +1,26 @@
 <template>
-  <div>Something</div>
+  <div>
+    <TechnicalSheet v-for="technicalsheet in technicalSheetList" :key="technicalsheet" :technical-sheet="technicalsheet" />
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
+import TechnicalSheet from './TechnicalSheet'
 export default {
+  components: {
+    TechnicalSheet
+  },
   data: () => ({
     technicalSheetList: []
   }),
   mounted () {
-    axios.get('localhost:3000/api/v1/technicalsheet/getall', {
+    axios.get('http://localhost:3000/api/v1/technicalsheet/getall', {
 
     }).then(response => {
       response.data.forEach(e => {
         this.technicalSheetList.push(e)
+        console.log(response.data)
       })
     })
   }
