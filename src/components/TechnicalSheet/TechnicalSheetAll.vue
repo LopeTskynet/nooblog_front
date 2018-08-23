dc5d5b589ff203ae7b9d5cf28f7b4dec22a6abe7
DylanBourdere <loperanes@gmail.com>
2018-08-23 22:08:09 +0200

    Add TechnicalSheetAll

---
+++ src/components/TechnicalSheet/TechnicalSheetAll.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>Something</div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data: () => ({
+    technicalSheetList: []
+  }),
+  mounted () {
+    axios.get('localhost:3000/api/v1/technicalsheet/getall', {
+
+    }).then(response => {
+      response.data.forEach(e => {
+        this.technicalSheetList.push(e)
+      })
+    })
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
